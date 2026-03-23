4cae19d6dfc8770d902af35a14b642a1f4d41d8a
Delta Celeste <130301897+DeltaCeleste@users.noreply.github.com>
2026-03-23 20:10:03 +0100

    pa pasarlo al sobremesa

--- models/key/Key.js
+++ models/key/Key.js
@@ -10,7 +10,14 @@ class Key extends THREE.Object3D {
     this.createGUI(gui,titleGui);
     
     // El material se usa desde varios métodos. Por eso se alamacena en un atributo
-    this.material = new THREE.MeshStandardMaterial({color: 0x885500});
+    //this.material = new THREE.MeshStandardMaterial({color: 0x885500});
+    this.material = new THREE.MeshStandardMaterial({
+      color: 0x886d32,
+      emissive: 0x463f28,
+      roughness: 0.5,
+      metalness: 1,
+      emissiveIntensity: 0.4,
+    });
     
     // A la base no se accede desde ningún método. Se almacena en una variable local del constructor
     var radio = 0.010;   
@@ -19,6 +26,7 @@ class Key extends THREE.Object3D {
     
     // Al nodo  this, la grapadora, se le cuelgan como hijos la base y la parte móvil
     this.add (base);
+
   }
   
   createGUI (gui,titleGui) {
@@ -36,72 +44,122 @@ class Key extends THREE.Object3D {
       .onChange ( (value) => this.setAngulo (-value) );
   }
 
-    createBody(radio, altura) {
-      // El nodo base
-      var base = new THREE.Object3D();
-
-      var radio_tubo = radio/5;
-
-      // El aro de la llave
-      var handle = new THREE.Mesh(new THREE.TorusKnotGeometry(radio, radio_tubo, 300, 20, 2, 11), this.material); //su radio total es radio+radio/2+radio_tubo, de ancho radio/2+radio_tubo 
-      // vamos a apoyarla sobre la mesa para mejor visualización
-      handle.rotation.x = Math.PI/2;
-      handle.position.y = radio/2+radio_tubo;
-
-      // El cuerpo principal
-      var neck = new THREE.Mesh(new THREE.CylinderGeometry(radio_tubo, radio_tubo, altura, 40, 1, false), this.material);
-      // Una punta redondeada para el cuerpo
-      var point = new THREE.Mesh(new THREE.SphereGeometry(radio_tubo, 20, 20), this.material);
-      point.position.y = altura/2;
-      neck.add(point);
-
-      // Lo apoyamos y desplazamos para unirla satisfactoriamente con el aro
-      neck.rotation.x = Math.PI/2;
-      neck.position.z = altura/2+radio/2+radio_tubo;
-      neck.position.y = radio/2+radio_tubo;
-
-      base.add(handle);
-      base.add(neck);
-      return base;
-    }
-
-    createBase(tama) {
-    // El nodo del que van a colgar la caja y los 2 conos y que se va a devolver
+  createBody(radio, altura) {
+    // El nodo base
     var base = new THREE.Object3D();
-    // Cada figura, un Mesh, está compuesto de una geometría y un material
-    var cajaBase = new THREE.Mesh (new THREE.BoxGeometry (tama,tama*0.08,tama*0.2), this.material);
-    cajaBase.position.y = tama*0.04;
-    // La componente geometría se puede compartir entre varios meshes
-    var geometriaPivote = new THREE.ConeGeometry (tama*0.05, tama*0.12);
-    var pivote1 = new THREE.Mesh (geometriaPivote, this.material);
-    var pivote2 = new THREE.Mesh (geometriaPivote, this.material);
-    // Se posicionan los pivotes con respecto a la base
-    pivote1.position.set (tama*0.45, tama*0.14, tama*0.05);
-    pivote2.position.set (tama*0.45, tama*0.14, -tama*0.05);
-    base.add(cajaBase);
-    base.add(pivote1);
-    base.add(pivote2);
+
+    var radio_tubo = radio/5;
+
+    // El aro de la llave
+    /*var handle = new THREE.Mesh(new THREE.TorusKnotGeometry(radio, radio_tubo, 300, 20, 2, 11), this.material); //su radio total es radio+radio/2+radio_tubo, de ancho radio/2+radio_tubo 
+    // vamos a apoyarla sobre la mesa para mejor visualización
+    handle.rotation.x = Math.PI/2;
+    handle.position.y = radio/2+radio_tubo;*/
+    var handle_shape = this.handle_deline();
+    var pts = [
+      new THREE.Vector3(10,0,0),
+      new THREE.Vector3(0,0,10),
+      new THREE.Vector3(-10,0,0),
+      new THREE.Vector3(0,0,-10),
+    ]
+    var path = new THREE.CatmullRomCurve3(pts, true);
+    var handle_options = {depth: 10, steps: 20, bevelEnabled: false, extrudePath: path};
+    var handle = new THREE.Mesh(new THREE.ExtrudeGeometry (handle_shape, handle_options), this.material);
+    handle.scale.z = (radio_tubo/3);
+    handle.scale.y = altura/3;
+    handle.scale.x = altura/3;
+
+    // El cuerpo principal
+    var neck = new THREE.Mesh(new THREE.CylinderGeometry(radio_tubo, radio_tubo, altura, 40, 1, false), this.material);
+    // Una punta redondeada para el cuerpo
+    var point = new THREE.Mesh(new THREE.SphereGeometry(radio_tubo), this.material);
+    point.position.y = altura/2;
+    neck.add(point);
+
+    //Dientes
+    var teethe_shape = this.teeth_deline();
+    var teeth_options = {depth: 1, steps: 2, bevelEnabled: false};
+    var teeth = new THREE.Mesh(new THREE.ExtrudeGeometry (teethe_shape, teeth_options), this.material);
+    teeth.scale.z = (radio_tubo/3);
+    teeth.scale.y = altura/4;
+    teeth.scale.x = altura/3;
+
+    //Lo posicionamos para unirlo en el extremo del cuerpo
+    teeth.position.y = altura/5;
+    //neck.add(teeth);
+
+    // Lo apoyamos y desplazamos para unirla satisfactoriamente con el aro
+    neck.rotation.x = Math.PI/2;
+    neck.position.z = altura/2+radio/2+radio_tubo;
+    neck.position.y = radio/2+radio_tubo;
+
+    base.add(handle);
+    //base.add(neck);
     return base;
   }
-  
-  createMovil (tama) {
-    // Se crea la parte móvil
-    var cajaMovil = new THREE.Mesh (
-        new THREE.BoxGeometry (tama, tama*0.12, tama*0.2),
-        this.material
-    );
-    cajaMovil.position.set (-tama*0.45, tama*0.06, 0);
-    
-    var movil = new THREE.Object3D();
-    // IMPORTANTE: Con independencia del orden en el que se escriban las 2 líneas siguientes, SIEMPRE se aplica primero la rotación y después la traslación. Prueba a intercambiar las dos líneas siguientes y verás que no se produce ningún cambio al ejecutar.    
-    movil.rotation.z = this.guiControls.rotacion;
-    movil.position.set(tama*0.45,tama*0.2,0);
-    movil.add(cajaMovil);
-    return movil;
+
+  teeth_deline(){
+    var shape = new THREE.Shape();
+    shape.moveTo(0,0);
+    shape.lineTo(0,-1);
+    shape.lineTo(1/3,-1);
+    shape.lineTo(1/3,-2/3);
+    shape.lineTo(4/6,-2/3);
+    shape.lineTo(4/6, -1);
+    shape.lineTo(1, -1);
+    shape.lineTo(1, -2/7);
+    shape.lineTo(3/8, -2/7);
+    shape.lineTo(3/8, 2/7);
+    shape.lineTo(7/8, 2/7);
+    shape.lineTo(7/8, 3/7);
+    shape.lineTo(4/8, 3/7);
+    shape.lineTo(4/8, 4/7);
+    shape.lineTo(1, 4/7);
+    shape.lineTo(1, 1);
+    shape.lineTo(0, 1);
+
+    shape.lineTo(0,);
+
+    return shape;
   }
-  
-  setAngulo (valor) {
-    this.movil.rotation.z = valor;
+
+  handle_deline(){
+    var shape = new THREE.Shape();
+    shape.lineTo(1,0);
+    shape.quadraticCurveTo(
+      1, 0.5,
+      0.5, 0.5
+    )
+    shape.quadraticCurveTo(
+      0.5, 1,
+      0, 1
+    )
+    shape.quadraticCurveTo(
+      -0.5, 1,
+      -0.5, 0.5
+    )
+    shape.quadraticCurveTo(
+      -1, 0.5,
+      -1, 0
+    )
+    shape.quadraticCurveTo(
+      -1, -0.5,
+      -0.5, -0.5
+    )
+    shape.quadraticCurveTo(
+      -0.5, -1,
+      0, -1
+    )
+    shape.quadraticCurveTo(
+      0.5, -1,
+      0.5, -0.5
+    )
+    shape.quadraticCurveTo(
+      1, -0.5,
+      1, 0
+    )
+
+    return shape
   }
   
   update () {
--- models/key/MyScene.js
+++ models/key/MyScene.js
@@ -38,7 +38,7 @@ class MyScene extends THREE.Scene {
     this.createCamera ();
 
     // Un suelo
-    this.createGround ();
+    //this.createGround ();
 
     // Y unos ejes. Imprescindibles para orientarnos sobre dónde están las cosas
     // Todas las unidades están en metros
